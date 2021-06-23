f24f64d1c25ea6b6ce966adb98008e50c9f85d23
akash8864 <akash.yadav@technocodz.com>
2021-06-23 22:53:46 +0530

    backend

---
+++ dummy-login-signup-without-redux/again-login-signup-without-redux/backend/routes/auth.routes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const userSchema = require("../models/users");
+
+router.post("/register", (req, res) => 
+{
+    const {name,email,password}=req.body;
+    userSchema.findOne({email:email},(err,user)=>
+    {
+        if(user)
+        {
+            res.send({message:"user already registered"})
+        }
+        else
+        {
+            const user=new userSchema({name,email,password})
+    user.save((err)=>{
+        if(err)
+        {
+            res.send(err)
+        }
+        else
+        {
+            res.send({message:"successfully saved"})
+        }
+    })
+        }
+    })
+});
+
+
+router.post(
+  "/login",(req,res)=>
+  {
+    const {email,password}=req.body; 
+    
+    userSchema.findOne({email:email},(err,user)=>
+    {
+        if(user)
+        {
+            if(password==user.password)
+            {
+                res.send({message:"user successfull",user})
+            }
+            else
+            {
+                res.send({message:"password not match"})
+            }
+        }
+        else
+        {
+            res.send({message:"user not registered"})
+        }
+    })
+
+
+
+  });
+
+
+module.exports = router;
