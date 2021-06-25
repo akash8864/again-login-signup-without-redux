afafaff20c5ddbd8896e5daa8fbf4a8a24e60007
akash8864 <akash.yadav@technocodz.com>
2021-06-25 14:55:26 +0530

    updated

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,58 +1,53 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import '../login/Login.css'
 import axios from 'axios'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom'
-function Login({loginuser}) {
+function Login({ loginuser }) {
     const notify = () => toast("Your Loggedin");
     const notify2 = () => toast("Please enter details");
-    const history=useHistory()
+    const history = useHistory()
     const [User, setUser] = useState({
-        email:"",
-        password:""
+        email: "",
+        password: ""
     })
-    const change=e=>
-    {
-       setUser({...User,[e.target.name]:e.target.value})
+    const change = e => {
+        setUser({ ...User, [e.target.name]: e.target.value })
     }
 
-    const submit=()=>
-    {
-        const { email,password}=User
-        if(email && password)
-        {
+    const submit = () => {
+        const { email, password } = User
+        if (email && password) {
             notify()
-           axios.post("/api/login",User).then((user)=>
-           {
-            setTimeout(() => {
-                loginuser(user.data.user)
-               history.push("/")
-            }, 1000);
-             
+            axios.post("/api/login", User).then((user) => {
+                setTimeout(() => {
+                    loginuser(user.data.user)
+                    history.push("/")
+                }, 1000);
+
             })
         }
-        else
-        {
+        else {
             notify2()
-        }  
+        }
     }
     return (
         <div className="login">
             <h1>Login</h1>
-            <br/>
-    <label htmlFor="email"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="email" value={User.email} onChange={change}/>
-    <label htmlFor="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" value={User.password} onChange={change} />
-    <br/>
-    <button className="button" type="submit" onClick={submit}>Login</button>
-    <ToastContainer toastClassName={"bg-success text-white font-weight-bold"}/>
-    <br/>
-    <span>OR</span>
-    <br/>
-<button type="button" className="button" onClick={()=>history.push("/signup")}>Register</button>
-<br/>
+            <br />
+            <label htmlFor="email"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="email" value={User.email} onChange={change} />
+            <label htmlFor="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" value={User.password} onChange={change} />
+            <br />
+            <button className="button" type="submit" onClick={submit}>Login</button>
+            <ToastContainer toastClassName={"bg-success text-white font-weight-bold"} />
+            <br />
+            <span>OR</span>
+            <br />
+            <button type="button" className="button" onClick={() => history.push("/signup")}>Register</button>
+            <br />
         </div>
     )
 }
